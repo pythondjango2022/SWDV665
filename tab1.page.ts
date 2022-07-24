91f108c896de9a40f562fba46c4732b8769eaecf
pythondjango2022 <101532744+pythondjango2022@users.noreply.github.com>
2022-07-23 19:26:45 -0500

    Week 4 Updated code

--- tab1.page.ts
+++ tab1.page.ts
@@ -45,23 +45,100 @@ export class Tab1Page {
 
   ];
 
-  constructor(public navCtrl: NavController, public toastController: ToastController, private alertController: AlertController) {}
+  constructor(public navCtrl: NavController, public toastController: ToastController, public alertController: AlertController) {}
 
-  async removeItem(item: any) {
-
+  async removeItem(item: any, index) {
+    
     console.log("Message here", item);
     const toast = await this.toastController.create({
-      message: 'The file has been deleted.' + item.name + "...",
+      message: 'The file has been deleted.' + item.name + item.quantity + "...",
       duration: 2000
     });
     toast.present();
+
+    this.items.splice(item,1)
   }
+
+
+
+  async editItem(item, index) {
+    
+    console.log("Edit message here - this is working", item);
+    const toast = await this.toastController.create({
+      message: "Editing item " + index + "",
+      duration: 2000
+    });
+    toast.present();
+    this.showEditItemPrompt(item, index);
+
+  }
+
+  async showEditItemPrompt(item, index) {
+    const alert = await this.alertController.create({
+      header: 'Edit Item',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Enter Grocery Item Name',
+          value: item.name,
+        },
+  
+        {
+          name: 'quantity',
+          type: 'number',
+          placeholder: 'Enter Quantity',
+          value: item.quantity,
+          min: 1,
+          max: 100
+        },
+      ],
+      buttons: [
+        {
+        text: 'Cancel',
+        handler: data => {
+          console.log("Cancel clicked");
+        }
+      },
+  
+      {
+        text: 'Save',
+        handler: item => {
+          console.log('Save clicked', item);
+        this.items[index] = item;
+        }
+      }
+  
+      ]
+      
+      
+  
+      
+    });
+    
+    await alert.present();
+    console.log(this.items)
+    
+  }
+  
+
+
+
+
 
 addItem() {
 
   console.log("Adding an item");
   this.showAddItemPrompt();
 }
+
+
+
+
+
+
+
+
+
 
 async showAddItemPrompt() {
   const alert = await this.alertController.create({
@@ -78,6 +155,7 @@ async showAddItemPrompt() {
       text: 'Save',
       handler: item=> {
         console.log("Saved clicked", this.items.push(item));
+        console.log("Details", + item.name + item.quantity + "");
       }
     }
 
@@ -106,6 +184,10 @@ async showAddItemPrompt() {
 
 }
 function item(item: any): any {
+  throw new Error('Function not implemented.');
+}
+
+function index(index: any, arg1: number) {
   throw new Error('Function not implemented.');
 }
 
